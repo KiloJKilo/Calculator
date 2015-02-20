0d9802883c0c29c0166b066d2a1e3d1fb46942f1
Kevin <kilojkilo@gmail.com>
2015-02-20 13:01:23 -0600

    mapped the numberpad key events in view class including opperands to the key listener in controller

--- Calculator/src/com/kevinjkahl/calculator/controller/Controller.java
+++ Calculator/src/com/kevinjkahl/calculator/controller/Controller.java
@@ -94,18 +94,62 @@ public class Controller extends KeyAdapter implements ActionListener {
 
 	// method to handle key events
 	public void keyPressed( KeyEvent e ) {
-
+//TODO: send each key event to the action listener or the respective method for the key/number
 		char key = e.getKeyChar();
 
 		switch ( key ) {
+		case '\n':
+			System.out.println( "Equals" );
+			break;
+		case '+':
+			System.out.println( "Addition" );
+			break;
+		case '-':
+			System.out.println( "Subtraction" );
+			break;
+		case '/':
+			System.out.println( "Division" );
+			break;
+		case '*':
+			System.out.println( "Multiplication" );
+			break;
 		case '1':
 			System.out.println( "1" );
 			break;
-
+		case '2':
+			System.out.println( "2" );
+			break;
+		case '3':
+			System.out.println( "3" );
+			break;
+		case '4':
+			System.out.println( "4" );
+			break;
+		case '5':
+			System.out.println( "5" );
+			break;
+		case '6':
+			System.out.println( "6" );
+			break;
+		case '7':
+			System.out.println( "7" );
+			break;
+		case '8':
+			System.out.println( "8" );
+			break;
+		case '9':
+			System.out.println( "9" );
+			break;
+		case 'c':
+		    System.out.println( "Clear" );
+			break;
+		case '':
+			System.out.println( "Clear Everything" );
+			break;
 		default:
 			break;
 		}
-		
+
 	}
 
 }
